db161a1c2d2a57def6aeb596ff28d7193f1a9b2c
Anton Kasheuski <kascheikinen@gmail.com>
2022-04-15 20:25:43 +0300

    Commit 4

--- src/projects/Project.tsx
+++ src/projects/Project.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from "./Project.module.css";
 
 type PropsType = {
+    id: number
     style: { backgroundImage: string }
     title: string
     description: string
 }
 
 const Project = (props: PropsType) => {
+    const [point, setPoint] = useState(false)
+    const [pointImg, setPointImg] = useState(false)
+
+    const onPointerHandler = (value: boolean) => {
+        setPoint(value)
+    }
+    const onPointerImgHandler = (value: boolean) => {
+        setPointImg(value)
+    }
+
     return (
-        <div className={style.projectBlock}>
-            <div className={style.projectImg} style={props.style}>
-                <button className={style.button}>open project</button>
+        <div className={style.projectBlock}
+             onPointerEnter={() => onPointerHandler(true)}
+             onPointerLeave={() => onPointerHandler(false)}
+        >
+            <div
+                className={style.projectImg + " " + (pointImg ? style.onPointImgEnter : style.onPointImgLeave)}
+                style={props.style}
+                onPointerEnter={() => onPointerImgHandler(true)}
+                onPointerLeave={() => onPointerImgHandler(false)}
+            >
             </div>
             <div className={style.projectText}>
-                <h5 className={style.projectHeader}>
+                <h5 className={style.projectHeader + " " + (point ? style.onPoint : "")}>
                     <a>{props.title}</a>
                 </h5>
                 <div className={style.projectDescription}>
--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -5,7 +5,7 @@ import Project from "./Project";
 import Title from "../common/components/title/Title";
 import socialImage from './../assets/image/social-net.jpeg'
 import todoImage from './../assets/image/todolist.jpeg'
-
+import fridayImage from './../assets/image/friday.jpeg'
 
 const Projects = () => {
     const social = {
@@ -14,6 +14,30 @@ const Projects = () => {
     const todolist = {
         backgroundImage: `url(${todoImage})`
     }
+    const friday = {
+        backgroundImage: `url(${fridayImage})`
+    }
+
+    const projects = [
+        {
+            id: 1,
+            style: social,
+            title: 'Social Network',
+            description: 'Dolore, porro rem endus magni, quas. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas. Lorem ipsum dolor sit amet, consectetur adipisicing elit.Dolore, porro rem quod illo'
+        },
+        {
+            id: 2,
+            style: todolist,
+            title: 'Todo List',
+            description: 'React\'s new "hooks" APIs give function components the ability to use local component state, execute side effects, and more. React also lets us write custom hooks, which let us extract reusable hooks to add our own behavior on top of React\'s built-in hooks.'
+        },
+        {
+            id: 3,
+            style: friday,
+            title: 'Friday Project',
+            description: 'Search the world\'s information, including webpages, images, videos and more. Google has many special features to help you find exactly what you\'re looking ...'
+        },
+    ]
 
     return (
         <div className={style.projectsBlock}>
@@ -23,16 +47,15 @@ const Projects = () => {
                     secondWord={"Projects"}
                 />
                 <div className={style.myProjects}>
-                    <Project
-                        style={social}
-                        title={'Social Network'}
-                        description={'Dolore, porro rem endus magni, quas. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, porro rem quod illo quam, eum alias id, repellendus magni, quas. Lorem ipsum dolor sit amet, consectetur adipisicing elit.Dolore, porro rem quod illo'}
-                    />
-                    <Project
-                        style={todolist}
-                        title={'Todo List'}
-                        description={'React\'s new "hooks" APIs give function components the ability to use local component state, execute side effects, and more. React also lets us write custom hooks, which let us extract reusable hooks to add our own behavior on top of React\'s built-in hooks.'}
-                    />
+                    {projects.map(pr =>
+                        <Project
+                            key={pr.id}
+                            id={pr.id}
+                            style={pr.style}
+                            title={pr.title}
+                            description={pr.description}
+                        />
+                    )}
                 </div>
             </div>
         </div>
