2f6a1dd5718acf9667cd7307658385650cc31dcf
Anton Kasheuski <kascheikinen@gmail.com>
2022-04-19 14:49:48 +0300

    Commit 9

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -5,30 +5,60 @@ import PersonIcon from '@mui/icons-material/Person';
 import PsychologyIcon from '@mui/icons-material/Psychology';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import CallIcon from '@mui/icons-material/Call';
+import Tooltip from '@mui/material/Tooltip';
 
 const Nav = () => {
+
+    const tooltipStyle = {
+        tooltip: {
+            sx: {
+                color: "#fff",
+                backgroundColor: "#6c54c7",
+                fontSize: "1rem",
+                fontFamily: "Lato,sans-serif",
+                lineHeight: "120%",
+                fontWeight: "700",
+                borderRadius: "0.5rem",
+                right: "10px",
+                padding: "10px 15px 10px 15px",
+            }
+        },
+        arrow: {
+            sx: {
+                color: "#6c54c7",
+            }
+        },
+    }
+
     return (
         <nav className={style.nav}>
             <NavLink to='/about' className={navData => navData.isActive ? style.activeIcon : style.icon}>
                 <div className={style.iconBox}>
-                    <PersonIcon sx={{ fontSize: 40 }}
-                    />
+                    <Tooltip title="About" placement="left" arrow componentsProps={tooltipStyle}
+                    >
+                        <PersonIcon sx={{fontSize: 40}}/>
+                    </Tooltip>
                 </div>
             </NavLink>
             <NavLink to='/skills' className={navData => navData.isActive ? style.activeIcon : style.icon}>
                 <div className={style.iconBox}>
-                    <PsychologyIcon sx={{ fontSize: 40 }}/>
+                    <Tooltip title="Skills" placement="left" arrow componentsProps={tooltipStyle}>
+                        <PsychologyIcon sx={{fontSize: 40}}/>
+                    </Tooltip>
                 </div>
             </NavLink>
             <NavLink to='/projects' className={navData => navData.isActive ? style.activeIcon : style.icon}>
                 <div className={style.iconBox}>
-                    <BusinessCenterIcon sx={{ fontSize: 40 }}/>
-                    <div className={style.navText}>Projects</div>
+                    <Tooltip title="Projects" placement="left" arrow componentsProps={tooltipStyle}>
+                        <BusinessCenterIcon sx={{fontSize: 40}}/>
+                    </Tooltip>
                 </div>
             </NavLink>
             <NavLink to='/contact' className={navData => navData.isActive ? style.activeIcon : style.icon}>
                 <div className={style.iconBox}>
-                    <CallIcon sx={{ fontSize: 40 }}/>
+                    <Tooltip title="Contact" placement="left" arrow componentsProps={tooltipStyle}>
+                        <CallIcon sx={{fontSize: 40}}/>
+                    </Tooltip>
                 </div>
             </NavLink>
         </nav>
